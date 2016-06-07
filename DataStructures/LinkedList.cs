2a074ce5ca0c30746d595d4113bc26350a86deaf
BotJeff <Jeffrey.E.Ferreiras@gmail.com>
2016-06-07 15:09:49 -0400

    'first'

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -10,21 +10,14 @@ namespace DataStructures
     {
         static void ExampleLinkedList()
         {
-            //
-            // Create a new linked list object instance.
-            //
+            //.NET has a default linked list class which takes generics.
             LinkedList<string> linked = new LinkedList<string>();
-            //
-            // Use AddLast method to add elements at the end.
-            // Use AddFirst method to add element at the start.
-            //
+            
             linked.AddLast("cat");
             linked.AddLast("dog");
             linked.AddLast("man");
             linked.AddFirst("first");
-            //
-            // Loop through the linked list with the foreach-loop.
-            //
+            
             foreach(var item in linked)
             {
                 Console.WriteLine(item);
@@ -32,25 +25,16 @@ namespace DataStructures
         }
         static void Example2()
         {
-            //
-            // Create a new linked list.
-            //
+            
             LinkedList<string> linked = new LinkedList<string>();
-            //
-            // First add three elements to the linked list.
-            //
+            
             linked.AddLast("one");
             linked.AddLast("two");
             linked.AddLast("three");
-            //
-            // Insert a node before the second node (after the first node)
-            //
           
             LinkedListNode<string> node = linked.Find("one");
             linked.AddAfter(node, "inserted");
-            //
-            // Loop through the linked list.
-            //
+            
             foreach(var value in linked)
             {
                 Console.WriteLine(value);
