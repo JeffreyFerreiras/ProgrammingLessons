5cc91908a4a046eb18558aec0c6f82b08493a66f
BotJeff <Jeffrey.E.Ferreiras@gmail.com>
2016-06-10 16:51:18 -0400

    ++practice

--- Algorithms/BinarySearch.cs
+++ Algorithms/BinarySearch.cs
@@ -11,36 +11,6 @@ namespace Algorithms
         /*
         The binary search algo takes an ordered list of numbers and finds the target in O(log n)
         */
-
-        //public BinarySearch()
-        //{
-        //    List<int> randList = new List<int>();
-        //    var random = new Random();
-        //    for(int i = 0; i < 100; i++)
-        //    {
-        //        randList.Add(random.Next(100));
-        //    }
-        //    BinSearch(randList.OrderBy(o=>o).ToArray(), 99);
-        //}
-        //public int BinSearch(int[] arry, int target)
-        //{
-        //    int low=0;
-        //    int high=arry.Length-1;
-        //    int mid;
-
-        //    while(low<=high)
-        //    {
-        //        mid = (low + high) / 2;//get middle position.
-        //        if(arry[mid] < target)
-        //            low = mid + 1;
-        //        else if(arry[mid] > target)
-        //            high = mid - 1;
-        //        else
-        //            return mid;   
-        //    }
-        //    return -1; //error!
-        //}
-
         public BinarySearch()
         {
             List<int> randList = new List<int>();
@@ -59,40 +29,40 @@ namespace Algorithms
             while(left <= right)
             {
                 int mid = (left + right) / 2;
-                if(arry[mid] < target) left = mid + 1; //only used to average out mid
-                else if(arry[mid] > target) right = mid - 1; //only used to average out mid
-                else return mid;
+                if(arry[mid] < target) left = mid + 1; //to search the right side, assign mid to left.
+                else if(arry[mid] > target) right = mid - 1; //to search the left side, assign mid to right.
+                else return mid; //this means taht mid is on the correct index.
             }
             return -1;
         }
+        public int RecursiveBinSearch(int[] arry, int left, int right, int target)
+        {   //recursive bin search 
+            int mid = (left + right)/2; //get the middle
+            if(arry[mid] == target)     return mid;//middle is found!
+            if(arry[mid] < target)      return RecursiveBinSearch(arry, mid + 1, right, target);// search the right side of the array
+            if(arry[mid] > target)      return RecursiveBinSearch(arry, left, mid - 1, target); // search the left side of the array
+            return -1;//not found!
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //AGAIN!
-        public int BinSearch2(int[] arry, int target)
+        int RecursiveBin2(int[]a, int l, int r, int x)
         {
-            int l = 0, r = arry.Length-1;
-            while(l<=r)
+            int m = (l+r)/2;
+            if(a[m] == x) return m;
+            if(a[m] < x)  return RecursiveBinSearch(a, l, m + 1, x);
+            if(a[m] > x)  return RecursiveBinSearch(a, m - 1, r, x);
+            return -1;
+        }
+        int BinSearch2(int[] a, int x)
+        {
+            int left = 0, right = a.Length-1;
+            while(left<=right)
             {
-                int m = (l+r)/2;
-                if(arry[m] > target) l=m+1; // always compare the midpoint to the target.
-                else if(arry[m] < target) r=m-1;
-                else return m;
+                int mid = (left + right) / 2; // get mid point
+                if(a[mid] < x) left = mid + 1;
+                else if(a[mid] > x) right = mid -1;
+                else return mid;
             }
-            return -1; //not found.
+            return -1;
         }
     }
 }
--- Algorithms/QuickSort.cs
+++ Algorithms/QuickSort.cs
@@ -8,6 +8,22 @@ namespace Algorithms
 {
     class QuickSort
     {
+        /*
+        1st half.
+        1) Get pivot point. GetPivotPoint(arry, left, right)
+        2) If left is less than pivot, recur left to pivot - 1
+        3) If right if less than pivot, recur pivot + 1 to right
+        */
+        /*
+        2nd half
+        GetPivotPoint(arry, left, right)
+        1) iterate while left is less than or equal to right.
+             a)get mid point.
+             b)traverse left index until one value in the array that belongs to the right is found, while increasing left++
+             c)traverse right index until one value in the array that belongs to the left is found, while decreasing right--;
+             d)check if left is still lower than or equal to right, if it is, swap values.
+        2) return left;
+         */
         public QuickSort()
         {
             List<int> randList = new List<int>();
@@ -18,11 +34,11 @@ namespace Algorithms
             QuickSorter(arry, 0, randList.Count() - 1);
             ;
         }
-        public void QuickSorter(int [] arry, int left, int right)
+        public void QuickSorter(int[] arry, int left, int right)
         {
             int pivot = GetPartitionedPivot(arry, left, right); //gets new pivot point.
             if(left < pivot - 1)//sort left half         
-                QuickSorter(arry, left, pivot - 1);         
+                QuickSorter(arry, left, pivot - 1);
             if(pivot < right)// sort right half
                 QuickSorter(arry, pivot, right);
         }
@@ -33,10 +49,10 @@ namespace Algorithms
             {
                 while(arry[left] < pivot)//find element on left that should be on the right.              
                     left++;
-                
+
                 while(arry[right] > pivot)//find element on right that should be on left, then move on to swap.          
                     right--;
-                
+
                 if(left <= right)//quick check to make sure we can swap.
                 {
                     //swap them!
@@ -58,26 +74,52 @@ namespace Algorithms
             if(pivot < right) QuickSort2(arry, pivot, right);
         }
         private int GetPartitionedPivot2(int[] arry, int left, int right)
-        {    
-            while(left <= right) //continues until the right index and left side reach the center
+        {
+            while(left <= right) //continues until the right index and left side reaches index of the right side.
             {
                 int mid = arry[(left + right) / 2];//gets the middle point of the array
-                while(arry[left] >= mid) left++; //move left index right until it reaches a number that belongs on the right side.
-                while(arry[right] < mid) right--; //move right index to the left until it reaches a number that belongs on the right side.
-                if(arry[left] >= arry[right]) //confirm the right side is less than a number on the left side
+                while(arry[left] < mid) left++; //move left index right until it reaches a number that belongs on the right side.
+                while(arry[right] > mid) right--; //move right index to the left until it reaches a number that belongs on the right side.
+
+                if(left <= right) //check that the indexes still make sense. The left can't be higher than the right index duhhh.
                 {
                     //swap
                     int temp = arry[left];
                     arry[left] = arry[right];
                     arry[right] = temp;
-                    
+
                     //avoid infinite loop
                     left++;
                     right--;
-                    return left;
                 }
             }
-            return -1; //error!
+            return left; // new pivot
+        }
+
+        void QuickSort3(int[] a, int left, int right)
+        {
+            int pivot = GetQuickSort3Pivot(a, left, right);
+            if(left < pivot) QuickSort3(a, left, pivot -1);
+            if(right > pivot) QuickSort3(a, pivot +1, right);
+        }
+        private int GetQuickSort3Pivot(int[] a, int left, int right)
+        {
+            while(left <= right)
+            {
+                int mid = (left + right)/2;
+                while(a[left] < a[mid]) left++;
+                while(a[right] > a[mid]) right--;
+                if(left <= right)
+                {
+                    int t = a[right];
+                    a[right] = a[left];
+                    a[left] = t;
+                    
+                    right++;
+                    left--;
+                }
+            }
+            return left;
         }
     }
 }
