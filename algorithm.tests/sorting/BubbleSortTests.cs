3fe7232d55f8c8302302e2f292a28ab98889a0a5
Jeffrey Ferreiras <Jeffrey.E.Ferreiras@gmail.com>
2017-11-14 10:11:03 -0500

    new tests

---
+++ algorithm.tests/sorting/BubbleSortTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Algorithms.Tests
+{
+    using Algorithms;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BubbleSortTests
+    {
+
+        [Test]
+        public void Sort_BubbleSort_OrderedArray()
+        {
+            int[] arry = new int[1000];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                arry[i] = Helper.RandomNumber(i, 1000);
+            }
+
+            Bubble.BubbleSort(arry);
+
+            Assert.IsTrue(Helper.IsSortedArray(arry));
+        }
+    }
+}
---
+++ algorithm.tests/sorting/QuickSortTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Algorithms;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class QuickSortTests
+    {
+        [Test]
+        public void QuickSort_SortArray_SortedArray()
+        {
+            var arry = Helper.GetRandomizedArray(1000);
+
+            arry.QuickSort();
+
+            Assert.IsTrue(Helper.IsSortedArray(arry));
+        }
+
+        [Test]
+        public void QuickSort_SortArray2_SortedArray()
+        {
+            var arry = Helper.GetRandomizedArray(1000);
+
+            arry.QuickSort2();
+
+            Assert.IsTrue(Helper.IsSortedArray(arry));
+        }
+    }
+}
