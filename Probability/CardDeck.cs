fc4ad66b064bdf5a25d395f8fe9b4ee604dd078b
Jeffrey Ferreiras <Jeffrey.E.Ferreiras@gmail.com>
2017-11-30 12:38:37 -0500

    Probability

---
+++ Probability/CardDeck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probability
+{
+    //Deck of cards probability problem.
+    //P(n)
+    public class CardDeck
+    {
+        const double SUITS = 4;
+        const double TYPES = 13;
+        const double COUNT = TYPES * SUITS; //52
+
+        public double ProbabilityOfAJack()
+        {
+            //P(Jack)
+
+            double probabilityOfJack = SUITS / COUNT; // 4/52
+            return probabilityOfJack;
+        }
+
+        public double ProbabilityOfHeart()
+        {
+            double hearts = TYPES * 1 / COUNT;
+
+            return hearts;
+        }
+
+        public double ProbabilityOfHeartOrJack()
+        {
+            //P(Heart or Jack)
+            
+            double r = (TYPES + SUITS) -1 / COUNT; //-1 is for the overlap
+
+            return r;
+        }
+    }
+}
---
+++ Probability/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probability
+{
+    class Program
+    {
+        public delegate double Probability();
+
+        static void Main(string[] args)
+        {
+            Print();
+        }
+
+        static void Print()
+        {
+            var deck = new CardDeck();
+            foreach (var method in typeof(CardDeck).GetMethods())
+            {
+                Console.WriteLine($"{method.Name}: \t\t\t{method.Invoke(deck, null)}");
+            }
+        }
+    }
+}
