6ab5401caebcfd58ff833fcddce3690c29738325
Jeffrey E. Ferreiras <Jeffrey.E.Ferreiras@gmail.com>
2020-07-14 14:15:45 -0400

    dephfirstsearchimpl

--- Algorithms/Search/DephFirstSearch.cs
+++ Algorithms/Search/DephFirstSearch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.Graphs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,43 @@ namespace Algorithms.Search
 {
     public class DephFirstSearch
     {
-		public Employee Search(Employee employee, string employeeName)
+		/*
+			1. check if the current employee has matching name, if so, return that employee
+            2.
+
+		 */
+		public Employee Search(Employee employee, string employeeName, HashSet<Employee> empSet = null)
 		{
-			
-		}
+			if(employee is null)
+            {
+				throw new InvalidOperationException();
+            }
+
+			var seen = empSet ?? new HashSet<Employee>();
+
+            if (employee.Name.Equals(employeeName))
+            {
+                return employee;
+            }
+
+            Employee found = null;
+
+            foreach (var subordinate in employee.Subordinates)
+            {
+                if (!seen.Contains(subordinate))
+                {
+                    found = Search(subordinate, employeeName, seen);
+
+                    if(found is object)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            seen.Add(employee);
+
+            return found;
+        }
     }
 }
